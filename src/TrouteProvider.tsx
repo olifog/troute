b3de03d71543b4d92caa213617107890b0503fdb
olifog <oliverfogelin@gmail.com>
2024-08-08 04:55:24 +0100

    initial commit

---
+++ src/TrouteProvider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from ".";
+import React from "react";
+
+export const TrouteProvider = ({
+  children,
+}: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,60 @@
+import { QueryClient, useQuery, UseQueryResult } from "@tanstack/react-query";
+import { NextRequest, NextResponse } from "next/server";
+
+type QueryFunction = (...args: any[]) => Promise<any>;
+
+type Queries = Record<string, QueryFunction>;
+
+type TrouteResult<T extends Queries> = {
+  GET: (request: NextRequest) => Promise<NextResponse>;
+  troute: {
+    [K in keyof T]: {
+      action: T[K];
+      useQuery: (
+        input: Parameters<T[K]>[0]
+      ) => UseQueryResult<Awaited<ReturnType<T[K]>>, unknown>;
+    };
+  };
+};
+
+export const createTroute = <T extends Queries>(
+  queries: T
+): TrouteResult<T> => {
+  return {
+    GET: async (request: NextRequest) => {
+      const searchParams = request.nextUrl.searchParams;
+      const route = searchParams.get("route") || "";
+      const input = JSON.parse(searchParams.get("input") || "null");
+      const query = queries[route as keyof T];
+
+      if (!query) {
+        return NextResponse.json({ error: 'No matching query found' }, { status: 404 })
+      }
+      
+      return NextResponse.json(await query(input));
+    },
+    troute: Object.fromEntries(
+      Object.entries(queries).map(([queryName, query]) => [
+        queryName,
+        {
+          action: query,
+          useQuery: (input: Parameters<typeof query>[0]) => {
+            return useQuery({
+              queryKey: [queryName, input],
+              queryFn: async () => {
+                const res = await fetch(
+                  `/api/troute?route=${queryName}&input=${encodeURIComponent(
+                    JSON.stringify(input)
+                  )}`
+                );
+                return res.json() as Promise<Awaited<ReturnType<typeof query>>>;
+              },
+            });
+          },
+        },
+      ])
+    ) as TrouteResult<T>["troute"],
+  };
+};
+
+export const queryClient = new QueryClient();
