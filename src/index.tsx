6111f0b27ea31539abb5b59d30cbb3fa799cddfb
olifog <oliverfogelin@gmail.com>
2024-08-10 03:06:37 +0100

    full circle

--- src/index.tsx
+++ src/index.tsx
@@ -9,19 +9,14 @@ type Queries = Record<string, QueryFunction>;
 type TrouteResult<T extends Queries> = {
   GET: (request: NextRequest) => Promise<NextResponse>;
   troute: {
-    [K in keyof T]: {
-      action: T[K];
-      call: T[K];
-      useQuery: (input: {
-        params?: Parameters<T[K]>[0];
-        enabled?: boolean;
-      }) => UseQueryResult<Awaited<ReturnType<T[K]>>, unknown>;
-    };
+    [K in keyof T]: (input: {
+      params?: Parameters<T[K]>[0];
+      enabled?: boolean;
+    }) => UseQueryResult<Awaited<ReturnType<T[K]>>, unknown>;
   };
 };
 
 export const createTroute = <T extends Queries>(
-  actions: Record<string, Function>,
   queries: T
 ): TrouteResult<T> => {
   return {
@@ -43,38 +38,26 @@ export const createTroute = <T extends Queries>(
     troute: Object.fromEntries(
       Object.entries(queries).map(([queryName, query]) => [
         queryName,
-        {
-          call: query,
-          action: async (...args) => {
-            const action = actions[queryName];
-            if (typeof action === "function") {
-              return await action(...args);
-            }
-            console.warn(
-              `No server action found for ${queryName}. Falling back to a direct call.`
-            );
-            return query(...args);
-          },
-          useQuery: ({
-            params,
-            enabled = true,
-          }: {
-            params?: Parameters<typeof query>[0];
-            enabled?: boolean;
-          }) => {
-            return useQuery({
-              queryKey: [queryName, params],
-              queryFn: async () => {
-                const res = await fetch(
-                  `/api/troute?route=${queryName}&input=${encodeURIComponent(
-                    JSON.stringify(params)
-                  )}`
-                );
-                return res.json() as Promise<Awaited<ReturnType<typeof query>>>;
-              },
-              enabled,
-            });
-          },
+        ({
+          params,
+          enabled = true,
+        }: {
+          params?: Parameters<typeof query>[0];
+          enabled?: boolean;
+        }) => {
+          return useQuery({
+            queryKey: [queryName, params],
+            queryFn: async () => {
+              const res = await fetch(
+                `/api/troute?route=${queryName}${
+                  params &&
+                  `&input=${encodeURIComponent(JSON.stringify(params))}`
+                }`
+              );
+              return res.json() as Promise<Awaited<ReturnType<typeof query>>>;
+            },
+            enabled,
+          });
         },
       ])
     ) as TrouteResult<T>["troute"],
